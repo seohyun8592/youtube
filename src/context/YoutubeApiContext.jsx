7d648efef56c4aba3a898c161cc0764e6df1ba40
김서현 <shhs0902@naver.com>
2023-02-03 01:15:43 +0900

    update02

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,10 +1,11 @@
 import { createContext, useContext } from 'react';
+import { FakeYoutubeClient } from '../api/fakeYoutubeClient';
 import { Youtube } from '../api/youtube';
-import { YoutubeClient } from '../api/youtubeClient';
+// import { YoutubeClient } from '../api/youtubeClient';
 
 export const YoutubeContext = createContext();
 
-const client = new YoutubeClient();
+const client = new FakeYoutubeClient();
 const youtube = new Youtube(client);
 export function YoutubeApiProvider({ children }) {
   return (
